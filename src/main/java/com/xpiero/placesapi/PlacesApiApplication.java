c7fa03e848f8fe429b5ce81a7ace183f92ddabef
Jean Pierre Matteo <piero89@gmail.com>
2021-02-17 21:53:10 +0100

    first commit

---
+++ src/main/java/com/xpiero/placesapi/PlacesApiApplication.java
@@ -0,0 +1,13 @@
+package com.xpiero.placesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpiero/placesapi/PlacesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpiero.placesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
